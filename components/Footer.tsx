77104dc50cf34d90f338afa52baa8274f4d36d80
Jeremy <jpersing1999@gmail.com>
2023-05-20 13:58:52 -0500

    added licensing attribution for emojis

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,17 +4,27 @@ import { GoMarkGithub } from "react-icons/go";
 export default function Footer() {
   return (
     <footer className="footer items-center p-6 bg-neutral text-neutral-content">
-      <div className="items-center grid-flow-col">
+      <div>
         <p>
           <Link href={"/"} className="hover:text-blue-400">
-            wrkouts.com
+            wrkouts.xyz
           </Link>{" "}
           - Super Simple Fitness Tool
+        </p>
+        <p className="text-xs">
+          Emojis come from the{" "}
+          <Link
+            href={"https://twemoji.twitter.com/"}
+            className="hover:text-blue-400"
+            target="__blank"
+          >
+            Twemoji Project
+          </Link>
         </p>
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <Link target="__blank" href={"https://github.com/JeremyPersing/"}>
-          <GoMarkGithub className="text-3xl" />
+          <GoMarkGithub className="text-4xl" />
         </Link>
       </div>
     </footer>
